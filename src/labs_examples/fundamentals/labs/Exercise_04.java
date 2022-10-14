167c98f2cb7799ed2048cbc1402013c0920d945c
andrewscott <abscott009@gmail.com>
2022-10-14 10:06:55 -0400

    pushing session 6 ex_04

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -1,5 +1,7 @@
 package labs_examples.fundamentals.labs;
 
+import videos_source_code.oop.polymorphism.Scooter;
+
 /**
  * Fundamentals Exercise 4: My Second Program
  *
@@ -16,6 +18,39 @@ public class Exercise_04 {
         System.out.println("int i is: " + i);
 
         // write your code below
+
+        boolean blue = true;
+
+        System.out.println("The sky is blue:" + blue);
+
+        char letter = 'L';
+
+        System.out.println("The capital of Louisville is:" + letter);
+
+        byte chief = 117;
+
+        System.out.println("ID number for Master Chief: " + chief);
+
+        short zip = 18051;
+
+        System.out.println("My zip code is: " + zip);
+
+        int phone = 2998085;
+
+        System.out.println("My phone number is: " + phone);
+
+        long miles = 33900000L;
+
+        System.out.println("Approx distance to Mars is " + miles + "miles");
+
+        float km = 54600000f;
+
+        System.out.println("Approx distance to Mars is " + km + "km");
+
+        double inch = 2144904000000.00;
+
+        System.out.println("Approx distance to Mars is " + inch + "inches");
+
 
     }
 
