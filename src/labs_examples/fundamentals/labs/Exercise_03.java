e00f132878f36c46d63ef7cbb462bef6de2fe658
andrewscott <abscott009@gmail.com>
2022-10-14 10:14:54 -0400

    updating exercise 1 & 3

--- src/labs_examples/fundamentals/labs/Exercise_03.java
+++ src/labs_examples/fundamentals/labs/Exercise_03.java
@@ -17,6 +17,13 @@ public class Exercise_03 {
         System.out.println("Hello World!");
 
         // write code to print out "Check out my first program!" below
+        /* Andrew Scott Java Fundamentals Lab Exercise 03
+         */
+
+        System.out.println("Hello World!");
+
+        System.out.println("Check out my first program!");
+
 
     }
 
