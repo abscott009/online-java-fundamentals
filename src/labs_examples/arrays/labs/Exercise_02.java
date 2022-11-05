811616e9c13efa8538562e19906a6039e0a8ea98
andrewscott <abscott009@gmail.com>
2022-11-05 11:57:24 -0400

    Array Ex. 2 complete

--- src/labs_examples/arrays/labs/Exercise_02.java
+++ src/labs_examples/arrays/labs/Exercise_02.java
@@ -1,5 +1,7 @@
 package labs_examples.arrays.labs;
 
+import java.util.Scanner;
+
 /**
  *  More labs_examples.arrays
  *
@@ -8,6 +10,8 @@ package labs_examples.arrays.labs;
  *
  */
 
+
+
 public class Exercise_02 {
 
     public static void main(String[] args) {
@@ -15,6 +19,35 @@ public class Exercise_02 {
         int[] array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 
         // write code here
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number between 1 and 10: ");
+
+        int num = sc.nextInt();
+
+        for (int i = 0; i <= array.length-1; i++){
+            if (num == array[i]) {
+                System.out.println("The position of " + num + " is " + i);
+            }
+        }
+
+//        List<Integer> myInts = Arrays.stream(array).boxed().toList();
+//
+//        boolean numFound = false;
+//        for (Integer number : myInts) {
+//
+//            if (number == num) {
+//                System.out.println(myInts.indexOf(num));
+//                numFound = true;
+//                break;
+//            }
+//        }
+//
+//        if (!numFound) {
+//            System.out.println("Num not found.");
+//        }
+
+
 
 
     }
