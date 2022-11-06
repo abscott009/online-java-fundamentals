86efd3a8f19d7731ee42c72004363cf0985dc604
andrewscott <abscott009@gmail.com>
2022-11-06 14:24:49 -0500

    Array Ex. 6 complete

--- src/labs_examples/arrays/labs/Exercise_06.java
+++ src/labs_examples/arrays/labs/Exercise_06.java
@@ -17,25 +17,31 @@ public class Exercise_06 {
     public static void main(String[] args) {
         int[] nums = {45, 87, 62, 99, 1, 42, 12};
 
-
         int temp;
 
         // Question: in the for loop below, why do we divide "nums.length" by 2?
             //Andrew_Response: Dividing 'nums.length' by 2 limits the loop to 3 iterations.
         for (int i = 0; i < nums.length / 2; i++) {
+            temp = nums[i];
+            nums[i] = nums[nums.length - i - 1];
+            nums[nums.length - i - 1] = temp;
             // swap elements at indexes in array
             // you have two indices readily available for you to use "i" (which increments) and "nums.length"
             // you've also got this "temp" variable you can use to hold a value temporarily
            }
-        //for (int i = nums.length - 1; i >= 0; i--) {
 
-        System.out.print("Contents of array after for loop - ");
-        // print each element of the array to verify reverse order
-        for (int i : nums) {
+            System.out.print("Contents of array after for loop - ");
+            // print each element of the array to verify reverse order
+            for (int i : nums) {
 
-            System.out.print(i + " ");
+
+
+                    System.out.print(i + " ");
+                }
+
         }
         }
-    }
+
+
 
 
