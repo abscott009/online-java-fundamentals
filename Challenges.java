446778b896d6fad1c160d708cb9d7d1bac41d741
andrewscott <abscott009@gmail.com>
2022-11-30 20:50:11 -0500

    Objects Exercises complete

---
+++ Challenges.java
@@ -0,0 +1,21 @@
+public class Challenges {
+
+        public int sum(int a, int b){
+            return a+b;
+        }
+
+        //subtract
+        public int subtract(int a, int b) {
+            return a - b;
+        }
+
+        //multiply
+        public int multiply(int a, int b) {
+            return a * b;
+        }
+        //divide
+        public int divide(int a, int b) {
+            return a / b;
+        }
+    }
+}
