f25cfab8561184ab0c58a5dd33e05098495fcf51
andrewscott <abscott009@gmail.com>
2023-01-05 14:32:45 -0500

    Packages Exercise

--- src/labs_examples/packages/labs/package1/ClassOne.java
+++ src/labs_examples/packages/labs/package1/ClassOne.java
@@ -1,12 +1,28 @@
 package labs_examples.packages.labs.package1;
 
+import labs_examples.packages.labs.package2.ClassTwo;
+
 public class ClassOne {
 
-    protected String classOne(){
-        return "Package 1, ClassOne, Protected method.";
+
+
+    protected String classOne() {
+        String words = "Package 1, ClassOne, Protected method.";
+        return words;
     }
 
-    public String secondMethod(){
-        return classOne();
+    public String secondMethod() {
+        String print = "Public Statement.";
+        return print;
+    }
+
+    public static void main(String[] args) {
+        ClassOne one = new ClassOne();
+        ClassTwo two = new ClassTwo();
+
+        System.out.println(one.secondMethod());
+        System.out.println(two.classTwo());
+
+
     }
 }
